b6a88068bb9e31f913449703ab8d74301964ed4a
Boris <borya-hohlov@mail.ru>
2022-01-22 13:19:29 +0300

    Create News Card

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Актуальные новости {{ now }}</h2>
+    </div>
+
+    <app-news></app-news>
   </div>
 </template>
 
 <script>
-
+import AppNews from './AppNews'
 export default {
-
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        'Джо победил',
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, rerum.'
+      ]
+    }
+  },
+  components: {
+    'app-news' : AppNews
+  }
 }
 </script>
 
---
+++ src/AppNews.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card" :key="item">
+    <h3>{{ item }}</h3>
+    <button @click="isOpen = !isOpen" class="btn">Открыть</button>
+    <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam atque aut dignissimos officiis perspiciatis possimus sapiente totam ullam veniam.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      item: 'item',
+      isOpen: false
+    }
+  }
+}
+</script>
